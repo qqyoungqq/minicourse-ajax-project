c2e27836e5233ccfecd54950cdc65fba6b7a969e
qqyoungqq <qianyang@mail.usf.edu>
2015-07-06 13:33:25 -0400

    feat: Load related wikipedia articles

--- js/script.js
+++ js/script.js
@@ -21,17 +21,39 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl +'">');
 
-    // NYTimes AJAX request goes here
+    // Append NYTimes Articles to the page
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=1cad5a3e1d8ca427013b47aa6305852f:0:72435225';
 
     $.getJSON(nytimesUrl,function (data) {
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
         var articles = data.response.docs;
-        for(var i=1,len=articles.length;i<len;i++) {
+        for(var i=0,len=articles.length;i<len;i++) {
             var article = articles[i];
             $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>' + '<p>' + article.snippet + '</p>'+'</li>');
         }
     }).error(function() { $nytHeaderElem.text('New York Times Articles Could Not Be Loaded')});
+
+    //Append Wikipedia Articles to the page
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text('failed to get wikipedia resources');
+    },8000);
+
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    $.ajax( {
+        url: wikiUrl,
+        dataType: 'jsonp',
+        success: function(data) {
+            var wikiArticles = data[1];
+            var wikiArticleUrls = data[3];
+            for(var i=0,len=wikiArticles.length;i<len;i++) {
+                var wikiArticle = wikiArticles[i];
+                var wikiArticleUrl = wikiArticleUrls[i];
+                $wikiElem.append('<li>' + '<a href="'+wikiArticleUrl+'">' +  wikiArticle + '</a>' + '</li>');
+            };
+            clearTimeout(wikiRequestTimeout);
+        }
+    } );
 
     return false;
 };
