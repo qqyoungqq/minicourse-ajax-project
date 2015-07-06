8d6554a4ecb6a2755468a1664a48ff32b6145330
qqyoungqq <qianyang@mail.usf.edu>
2015-07-06 11:21:32 -0400

    feat: Load the recent New York Times Articles about the city

--- js/script.js
+++ js/script.js
@@ -21,9 +21,20 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl +'">');
 
+    // NYTimes AJAX request goes here
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=1cad5a3e1d8ca427013b47aa6305852f:0:72435225';
+
+    $.getJSON(nytimesUrl,function (data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        var articles = data.response.docs;
+        for(var i=1,len=articles.length;i<len;i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>' + '<p>' + article.snippet + '</p>'+'</li>');
+        }
+    }).error(function() { $nytHeaderElem.text('New York Times Articles Could Not Be Loaded')});
+
     return false;
 };
 
 $('#form-container').submit(loadData);
 
-// loadData();
